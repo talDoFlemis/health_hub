08abd56f161735a8171cc1228a14f61b31a4078d
taldoflemis <coderflemis@gmail.com>
2023-06-30 19:00:22 -0300

    feat: adicionar swagger
    adicionar SwaggerUI ao pom.xml
    
    adicionar SwaggerUI ao whitelist do SecurityConfiguration
    
    adicionar OpenAPIConfig

--- backend/src/main/java/com/usguri/health_hub/config/SecurityConfiguration.java
+++ backend/src/main/java/com/usguri/health_hub/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.usguri.health_hub.config;
 
-import com.usguri.health_hub.user.Role;
+import static com.usguri.health_hub.user.Role.*;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,41 +15,49 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
-import static com.usguri.health_hub.user.Role.*;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class SecurityConfiguration {
-    private final JwtAuthenticationFilter jwtAuthFilter;
-    private final AuthenticationProvider authenticationProvider;
-    private final LogoutHandler logoutHandler;
+  private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
+  private final LogoutHandler logoutHandler;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/auth/**")
-                .permitAll()
-                .requestMatchers("/api/patient")
-                .hasAnyRole(ADMIN.name(), PATIENT.name(), ATTENDANT.name())
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .logout()
-                .logoutUrl("/api/auth/logout")
-                .addLogoutHandler(logoutHandler)
-                .logoutSuccessHandler((request,response,authentication) -> SecurityContextHolder.clearContext());
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .csrf()
+        .disable()
+        .authorizeHttpRequests()
+        .requestMatchers(
+            "/api/auth/**",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html")
+        .permitAll()
+        .requestMatchers("/api/patient")
+        .hasAnyRole(ADMIN.name(), PATIENT.name(), ATTENDANT.name())
+        .anyRequest()
+        .authenticated()
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .logout()
+        .logoutUrl("/api/auth/logout")
+        .addLogoutHandler(logoutHandler)
+        .logoutSuccessHandler(
+            (request, response, authentication) -> SecurityContextHolder.clearContext());
 
-        return httpSecurity.build();
-    }
-
+    return httpSecurity.build();
+  }
 }
