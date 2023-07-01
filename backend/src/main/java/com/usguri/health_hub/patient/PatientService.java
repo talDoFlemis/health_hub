8cb1000ae7d08713ac29ed7ae35d7cff881d9be6
taldoflemis <coderflemis@gmail.com>
2023-07-01 14:09:54 -0300

    atualizar PatientService para usar DTOs

--- backend/src/main/java/com/usguri/health_hub/patient/PatientService.java
+++ backend/src/main/java/com/usguri/health_hub/patient/PatientService.java
@@ -1,51 +1,79 @@
 package com.usguri.health_hub.patient;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.persistence.EntityExistsException;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-
 @Service
+@RequiredArgsConstructor
 public class PatientService {
-    private final PatientRepository patientRepository;
+  private final PatientRepository patientRepository;
 
-    @Autowired
-    public PatientService(PatientRepository patientRepository) {
-        this.patientRepository = patientRepository;
+  public List<Patient> getAll() {
+    return this.patientRepository.findAll();
+  }
+
+  private Patient findUserById(Long id) {
+    return this.patientRepository
+        .findById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Patient with 'id':" + id + " not found."));
+  }
+
+  public Patient findById(Long id) {
+    return findUserById(id);
+  }
+
+  public Patient findByEmail(String email) {
+    return this.patientRepository
+        .findByEmail(email)
+        .orElseThrow(
+            () -> new EntityNotFoundException("Patient with 'email': " + email + " not found"));
+  }
+
+  public Patient createPatient(CreatePatientDTO dto) {
+    Patient pat =
+        Patient.builder()
+            .firstname(dto.getFirstname())
+            .lastname(dto.getLastname())
+            .email(dto.getEmail())
+            .dbo(dto.getDbo())
+            .build();
+    try {
+      return this.patientRepository.save(pat);
+    } catch (Exception e) {
+      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists");
+    }
+  }
+
+  @Transactional
+  public void removePatient(Long id) {
+    Patient pat = findUserById(id);
+    this.patientRepository.deleteById(pat.getId());
+  }
+
+  @Transactional
+  public Patient updatePatient(UpdatePatientDTO dto, Long id) {
+    Patient original = findUserById(id);
+    if (dto.getFirstname() != null) {
+      original.setFirstname(dto.getFirstname());
+    }
+    if (dto.getLastname() != null) {
+      original.setLastname(dto.getLastname());
+    }
+    if (dto.getDbo() != null) {
+      original.setDbo(dto.getDbo());
+    }
+    if (dto.getEmail() != null) {
+      original.setEmail(dto.getEmail());
     }
 
-    public List<Patient> getAll() {
-        return this.patientRepository.findAll();
-
+    try {
+      return this.patientRepository.save(original);
+    } catch (Exception e) {
+      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists");
     }
-
-    public Optional<Patient> findById(Long id) {
-        return this.patientRepository.findById(id);
-    }
-
-    public Optional<Patient> findByEmail(String email) {
-        return this.patientRepository.findByEmail(email);
-    }
-
-    public Patient createPatient(Patient patDTO) {
-        Optional<Patient> pat = this.findByEmail(patDTO.getEmail());
-        if (pat.isPresent()) {
-            throw new IllegalStateException("email taken");
-        } else {
-            return this.patientRepository.save(patDTO);
-        }
-    }
-
-    public void removePatient(Long id) {
-        this.patientRepository.deleteById(id);
-    }
-
-    public Patient updatePatient(Patient pat) {
-        Patient p= this.patientRepository.findById(pat.getId()).orElseThrow(() -> new IllegalStateException("Entry not found"));
-        p.setName(pat.getName());
-        p.setEmail(pat.getEmail());
-        p.setDbo(pat.getDbo());
-        return this.patientRepository.save(p);
-    }
+  }
 }
