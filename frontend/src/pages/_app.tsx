61b78a931bec035b6ee3acf3f174db4b3b4ec03a
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-07-02 17:12:39 -0300

    Add dashboard layout, Create index page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import '@/styles/globals.css'
+import { ReactElement, ReactNode } from "react"
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from "@chakra-ui/react"
+import { NextPage } from 'next'
 
-function App({ Component, pageProps }: AppProps) {
-  return (
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return ( 
     <ChakraProvider>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </ChakraProvider>
-  ) 
+  )
 }
 
 export default App
