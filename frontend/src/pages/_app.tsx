cdc21e8dfa8c4cecd1629159d5f4e6e465887246
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-07-02 00:07:10 -0300

    Setup chakra provider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  ) 
 }
+
+export default App
