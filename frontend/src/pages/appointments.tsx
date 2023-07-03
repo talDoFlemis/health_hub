0abc6fb19eb3491ab956410d04f198a1c83fd927
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-07-02 23:14:17 -0300

    commit para guardar progresso

--- frontend/src/pages/appointments.tsx
+++ frontend/src/pages/appointments.tsx
@@ -1,27 +1,50 @@
 import Head from "next/head";
 import {
     TableContainer,
-    Table, Thead, TableCaption,
+    Table, Thead, TableCaption, Th, Tr, extendTheme,
 } from "@chakra-ui/react";
 
 const consult = () => {
     return (<>
-        <Head>
-            <title>Marque sua consulta</title>
-        </Head>
         <main>
-            <div className="flex flex-column items-center justify-center">
-                <TableContainer className={`
-                    bg-
+            <div className="flex justify-center items-center h-screen w-full">
+                <div className={`
+                    bg-primary text-primary
+                    shadow-sm rounded-lg gap-8 p-6
                 `}>
-                    <Table variant={'simple'}>
-                        <Thead>
-                            <TableCaption>Marque Sua Consulta Aqui</TableCaption>
-                        </Thead>
-                    </Table>
-                </TableContainer>
+                    <div className={`
+                        flex justify-center 
+                        w-full
+                    `}>
+                        <span className="text-2xl text-boxText border-0">
+                            Marque sua consulta
+                        </span>
+                    </div>
+                    <div className={`
+                        bg-box
+                    `}>
+                        <Table>
+                            <Thead>
+                                <Tr color="white">
+                                    <Th><span
+                                        className={"text-boxText"}>Segunda Feira
+                                    </span></Th>
+                                    <Th><span
+                                        className={"text-boxText"}>Segunda Feira
+                                    </span></Th>
+                                    <Th><span
+                                        className={"text-boxText"}>Segunda Feira
+                                    </span></Th>
+                                    <Th><span
+                                        className={"text-boxText"}>Segunda Feira
+                                    </span></Th>
+                                </Tr>
+                            </Thead>
+                        </Table>
+                    </div>
+                </div>
             </div>
         </main>
-</>)};
+    </>);};
 
 export default consult;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       colors: {
         primary: "#461bff",
         secondary: "#0e0e5e",
+        box: "#B1B3C7",
+        boxText: "#FFFFFF",
         accent: "#FF9100",
         description: "#21262C",
       },
