e22395ece6c6bda78f9b0509e695b80d593568b2
taldoflemis <coderflemis@gmail.com>
2023-07-03 21:05:18 -0300

    atualizar SeedDatabase para ler do CSV

--- backend/src/main/java/com/usguri/health_hub/config/SeedDatabase.java
+++ backend/src/main/java/com/usguri/health_hub/config/SeedDatabase.java
@@ -1,8 +1,13 @@
 package com.usguri.health_hub.config;
 
+import com.usguri.health_hub.appointment.AppointmentRepository;
 import com.usguri.health_hub.patient.Patient;
 import com.usguri.health_hub.patient.PatientRepository;
-import java.time.LocalDate;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,20 +16,25 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SeedDatabase implements CommandLineRunner {
   private final PatientRepository patientRepository;
+  private final AppointmentRepository appointmentRepository;
+
+  private <T> List<T> getListFromCSV(Class<T> clazz, String fileName) throws IOException {
+    File file = new File(fileName);
+    InputStream stream = new FileInputStream(file);
+    return CsvUtils.read(clazz, stream);
+  }
 
   @Override
   public void run(String... args) throws Exception {
     loadUserData();
   }
 
-  private void loadUserData() {
+  private void loadUserData() throws IOException {
     if (patientRepository.count() == 0) {
-      Patient pat1 = new Patient("Lg", "Dos Santos", LocalDate.now().minusYears(19), "lg@test.com");
-      Patient pat2 =
-          new Patient("Tubias", "Souza", LocalDate.now().minusYears(30), "tubias@test.com");
-      patientRepository.save(pat1);
-      patientRepository.save(pat2);
+      List<Patient> patients = getListFromCSV(Patient.class, "src/main/resources/patients.csv");
+      patientRepository.saveAll(patients);
     }
-    System.out.println(patientRepository.count());
+    System.out.println("Patient count" + patientRepository.count());
+    System.out.println("Appointment count" + appointmentRepository.count());
   }
 }
