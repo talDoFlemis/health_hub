4b62227c2c3f93fe43e913a99c3b27747a318a05
taldoflemis <coderflemis@gmail.com>
2023-07-04 20:50:07 -0300

    adicionar next-auth, moment, swr como dependecy
    adicionar next-authjs as dependecy
    
    adicionar swr as dependencies
    
    adicionar moment como dependency

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ReactElement, ReactNode } from "react";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { SessionProvider } from "next-auth/react";
+import "moment/locale/pt-br";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
