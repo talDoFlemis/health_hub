16fe89c05a692c89b54f105c22ecce379505946e
taldoflemis <coderflemis@gmail.com>
2023-07-04 14:54:16 -0300

    adicionar Appointments ao SeedDatabase

--- backend/src/main/java/com/usguri/health_hub/config/SeedDatabase.java
+++ backend/src/main/java/com/usguri/health_hub/config/SeedDatabase.java
@@ -1,12 +1,17 @@
 package com.usguri.health_hub.config;
 
+import com.usguri.health_hub.appointment.Appointment;
+import com.usguri.health_hub.appointment.AppointmentRaw;
 import com.usguri.health_hub.appointment.AppointmentRepository;
 import com.usguri.health_hub.patient.Patient;
 import com.usguri.health_hub.patient.PatientRepository;
+import com.usguri.health_hub.physician.Physician;
+import com.usguri.health_hub.physician.PhysicianRepository;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -16,6 +21,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SeedDatabase implements CommandLineRunner {
   private final PatientRepository patientRepository;
+  private final PhysicianRepository physicianRepository;
   private final AppointmentRepository appointmentRepository;
 
   private <T> List<T> getListFromCSV(Class<T> clazz, String fileName) throws IOException {
@@ -29,12 +35,43 @@ public class SeedDatabase implements CommandLineRunner {
     loadUserData();
   }
 
+  boolean db_empty() {
+    return patientRepository.count() == 0 && physicianRepository.count() == 0;
+  }
+
   private void loadUserData() throws IOException {
+    boolean db_empty = db_empty();
     if (patientRepository.count() == 0) {
       List<Patient> patients = getListFromCSV(Patient.class, "src/main/resources/patients.csv");
       patientRepository.saveAll(patients);
     }
+    if (physicianRepository.count() == 0) {
+      List<Physician> physicians =
+          getListFromCSV(Physician.class, "src/main/resources/physicians.csv");
+      physicianRepository.saveAll(physicians);
+    }
+    if (db_empty) {
+      List<AppointmentRaw> appointmentRaws =
+          getListFromCSV(AppointmentRaw.class, "src/main/resources/appointments.csv");
+      List<Appointment> appointments = new ArrayList<>();
+      appointmentRaws.forEach(
+          a -> {
+            Patient patient = patientRepository.findById(a.getPatient_id()).orElseThrow();
+            Physician physician = physicianRepository.findById(a.getPhysician_id()).orElseThrow();
+            Appointment appointment =
+                Appointment.builder()
+                    .id(a.getId())
+                    .annotations(a.getAnnotations())
+                    .time(a.getTime())
+                    .patient(patient)
+                    .physician(physician)
+                    .build();
+            appointments.add(appointment);
+          });
+      appointmentRepository.saveAll(appointments);
+    }
     System.out.println("Patient count" + patientRepository.count());
+    System.out.println("Physician count" + physicianRepository.count());
     System.out.println("Appointment count" + appointmentRepository.count());
   }
 }
