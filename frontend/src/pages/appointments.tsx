da4a589742f75f78d6e0819e9660cd4b6e3e3a13
Pedro Vinicius Coelho belem <pevine6508@gmail.com>
2023-07-07 11:06:23 -0300

    organizando branch

--- frontend/src/pages/appointments.tsx
+++ frontend/src/pages/appointments.tsx
@@ -1,50 +1,23 @@
-import Head from "next/head";
-import {
-    TableContainer,
-    Table, Thead, TableCaption, Th, Tr, extendTheme,
-} from "@chakra-ui/react";
+import Head from 'next/head'
+import DashboardLayout from '@/layouts/DashboardLayout'
+import type { NextPageWithLayout } from './_app'
+import { ReactElement } from 'react'
 
-const consult = () => {
-    return (<>
-        <main>
-            <div className="flex justify-center items-center h-screen w-full">
-                <div className={`
-                    bg-primary text-primary
-                    shadow-sm rounded-lg gap-8 p-6
-                `}>
-                    <div className={`
-                        flex justify-center 
-                        w-full
-                    `}>
-                        <span className="text-2xl text-boxText border-0">
-                            Marque sua consulta
-                        </span>
-                    </div>
-                    <div className={`
-                        bg-box
-                    `}>
-                        <Table>
-                            <Thead>
-                                <Tr color="white">
-                                    <Th><span
-                                        className={"text-boxText"}>Segunda Feira
-                                    </span></Th>
-                                    <Th><span
-                                        className={"text-boxText"}>Segunda Feira
-                                    </span></Th>
-                                    <Th><span
-                                        className={"text-boxText"}>Segunda Feira
-                                    </span></Th>
-                                    <Th><span
-                                        className={"text-boxText"}>Segunda Feira
-                                    </span></Th>
-                                </Tr>
-                            </Thead>
-                        </Table>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </>);};
+const Appointments: NextPageWithLayout = () => {
+    return (
+        <>
+            <Head>
+                <title>Health Hub - Consultas</title>
+            </Head>
+            <main>
 
-export default consult;
+            </main>
+        </>
+    )
+}
+
+Appointments.getLayout = function getLayout(page: ReactElement) {
+    return <DashboardLayout>{page}</DashboardLayout>
+}
+
+export default Appointments
