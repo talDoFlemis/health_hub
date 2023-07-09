25dacd48b2408436eb7c385dd9363309d26c0bf6
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-07-08 23:34:09 -0300

    Renomear pagina de medicos

--- frontend/src/pages/doctors.tsx
+++ frontend/src/pages/doctors.tsx
@@ -8,7 +8,7 @@ const Doctors: NextPageWithLayout = () => {
   return (
     <>
       <Head>
-        <title>Health Hub - Doctors</title>
+        <title>Health Hub - Médicos</title>
       </Head>
       <main className="min-h-screen w-full py-4 px-2">
         <h1 className="text-primary text-5xl font-bold mb-4">Buscar</h1>
