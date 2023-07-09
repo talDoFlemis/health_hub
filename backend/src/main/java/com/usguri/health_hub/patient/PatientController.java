0f9e526a002eaac2b5dd20350d49f63f17d91cab
taldoflemis <coderflemis@gmail.com>
2023-07-09 12:29:26 -0300

    docs: adicionar docs ao package patient
    adicionar documentacao ao PatientService
    
    adicionar documentacao ao PatientController

--- backend/src/main/java/com/usguri/health_hub/patient/PatientController.java
+++ backend/src/main/java/com/usguri/health_hub/patient/PatientController.java
@@ -9,34 +9,71 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controlador responsável por lidar com as operações relacionadas aos pacientes.
+ *
+ * @author Said Rodrigues
+ * @version 1.0
+ * @since 2023-07-01
+ */
 @RestController
 @RequestMapping(path = "${apiPrefix}/patient")
 @RequiredArgsConstructor
 public class PatientController {
+
   private final PatientService patientService;
 
+  /**
+   * Obtém os dados do paciente autenticado.
+   *
+   * @param auth A autenticação do paciente.
+   * @return Os dados do paciente autenticado.
+   */
   @GetMapping("/me")
   public Patient getMyData(Authentication auth) {
     return this.patientService.findByEmail(auth.getName());
   }
 
+  /**
+   * Obtém a lista de consultas do paciente autenticado.
+   *
+   * @param auth A autenticação do paciente.
+   * @return A lista de consultas do paciente autenticado.
+   */
   @GetMapping("/me/appointments")
   public List<Appointment> getMyAppointments(Authentication auth) {
     return this.patientService.findAppointmentsByEmail(auth.getName());
   }
 
+  /**
+   * Obtém a lista de todos os pacientes (apenas para ADMIN ou ATTENDANT).
+   *
+   * @return A lista de todos os pacientes.
+   */
   @GetMapping("/all")
   @PreAuthorize(value = "hasAnyRole('ADMIN', 'ATTENDANT')")
   public List<Patient> getAllPatients() {
     return this.patientService.getAll();
   }
 
+  /**
+   * Obtém os dados de um paciente pelo seu ID (apenas para ADMIN ou ATTENDANT).
+   *
+   * @param id O ID do paciente.
+   * @return Os dados do paciente encontrado.
+   */
   @GetMapping("/{id}")
   @PreAuthorize(value = "hasAnyRole('ADMIN', 'ATTENDANT')")
   public Patient getPatientById(@PathVariable Long id) {
     return this.patientService.findById(id);
   }
 
+  /**
+   * Registra um novo paciente (apenas para ADMIN ou ATTENDANT).
+   *
+   * @param pat Os dados do paciente a ser registrado.
+   * @return O paciente registrado.
+   */
   @PostMapping("/create")
   @ResponseStatus(HttpStatus.CREATED)
   @PreAuthorize(value = "hasAnyRole('ADMIN', 'ATTENDANT')")
@@ -44,6 +81,11 @@ public class PatientController {
     return this.patientService.createPatient(pat);
   }
 
+  /**
+   * Remove um paciente pelo seu ID (apenas para ADMIN ou ATTENDANT).
+   *
+   * @param id O ID do paciente a ser removido.
+   */
   @DeleteMapping("{id}")
   @ResponseStatus(HttpStatus.OK)
   @PreAuthorize(value = "hasAnyRole('ADMIN', 'ATTENDANT')")
@@ -51,6 +93,13 @@ public class PatientController {
     this.patientService.removePatient(id);
   }
 
+  /**
+   * Atualiza os dados de um paciente pelo seu ID (apenas para ADMIN ou ATTENDANT).
+   *
+   * @param pat Os dados atualizados do paciente.
+   * @param id O ID do paciente a ser atualizado.
+   * @return Os dados do paciente atualizado.
+   */
   @PatchMapping("/update/{id}")
   @ResponseStatus(HttpStatus.OK)
   @PreAuthorize(value = "hasAnyRole('ADMIN', 'ATTENDANT')")
--- backend/src/main/java/com/usguri/health_hub/patient/PatientService.java
+++ backend/src/main/java/com/usguri/health_hub/patient/PatientService.java
@@ -9,33 +9,74 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Serviço responsável por realizar operações relacionadas aos pacientes.
+ *
+ * @author Said Rodrigues
+ * @version 1.0
+ * @since 2023-07-01
+ */
 @Service
 @RequiredArgsConstructor
 public class PatientService {
+
   private final PatientRepository patientRepository;
   private final AppointmentRepository appointmentRepository;
 
+  /**
+   * Obtém a lista de todos os pacientes.
+   *
+   * @return A lista de todos os pacientes.
+   */
   public List<Patient> getAll() {
     return this.patientRepository.findAll();
   }
 
+  /**
+   * Localiza um paciente pelo seu ID.
+   *
+   * @param id O ID do paciente.
+   * @return Os dados do paciente encontrado.
+   * @throws EntityNotFoundException Se o paciente não for encontrado.
+   */
   private Patient findUserById(Long id) {
     return this.patientRepository
         .findById(id)
-        .orElseThrow(() -> new EntityNotFoundException("Patient with 'id':" + id + " not found."));
+        .orElseThrow(() -> new EntityNotFoundException("Patient with 'id': " + id + " not found."));
   }
 
+  /**
+   * Obtém os dados de um paciente pelo seu ID.
+   *
+   * @param id O ID do paciente.
+   * @return Os dados do paciente encontrado.
+   * @throws EntityNotFoundException Se o paciente não for encontrado.
+   */
   public Patient findById(Long id) {
     return findUserById(id);
   }
 
+  /**
+   * Obtém os dados de um paciente pelo seu email.
+   *
+   * @param email O email do paciente.
+   * @return Os dados do paciente encontrado.
+   * @throws EntityNotFoundException Se o paciente não for encontrado.
+   */
   public Patient findByEmail(String email) {
     return this.patientRepository
         .findByEmail(email)
         .orElseThrow(
-            () -> new EntityNotFoundException("Patient with 'email': " + email + " not found"));
+            () -> new EntityNotFoundException("Patient with 'email': " + email + " not found."));
   }
 
+  /**
+   * Cria um novo paciente.
+   *
+   * @param dto Os dados do paciente a ser registrado.
+   * @return O paciente registrado.
+   * @throws EntityExistsException Se já existir um paciente com o mesmo email.
+   */
   public Patient createPatient(CreatePatientDTO dto) {
     Patient pat =
         Patient.builder()
@@ -47,10 +88,15 @@ public class PatientService {
     try {
       return this.patientRepository.save(pat);
     } catch (Exception e) {
-      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists");
+      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists.");
     }
   }
 
+  /**
+   * Remove um paciente pelo seu ID.
+   *
+   * @param id O ID do paciente a ser removido.
+   */
   @Transactional
   public void removePatient(Long id) {
     Patient pat = findUserById(id);
@@ -58,6 +104,14 @@ public class PatientService {
     this.patientRepository.deleteById(pat.getId());
   }
 
+  /**
+   * Atualiza os dados de um paciente pelo seu ID.
+   *
+   * @param dto Os dados atualizados do paciente.
+   * @param id O ID do paciente a ser atualizado.
+   * @return Os dados do paciente atualizado.
+   * @throws EntityExistsException Se já existir um paciente com o mesmo email.
+   */
   @Transactional
   public Patient updatePatient(UpdatePatientDTO dto, Long id) {
     Patient original = findUserById(id);
@@ -77,17 +131,24 @@ public class PatientService {
     try {
       return this.patientRepository.save(original);
     } catch (Exception e) {
-      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists");
+      throw new EntityExistsException("Patient with email: " + dto.getEmail() + " already exists.");
     }
   }
 
+  /**
+   * Obtém a lista de consultas de um paciente pelo seu email.
+   *
+   * @param email O email do paciente.
+   * @return A lista de consultas do paciente.
+   * @throws EntityNotFoundException Se o paciente não for encontrado.
+   */
   @Transactional
-  public List<Appointment> findAppointmentsByEmail(String name) {
+  public List<Appointment> findAppointmentsByEmail(String email) {
     Patient pat =
         this.patientRepository
-            .findByEmail(name)
+            .findByEmail(email)
             .orElseThrow(
-                () -> new EntityNotFoundException("Patient with email: " + name + " not found"));
+                () -> new EntityNotFoundException("Patient with email: " + email + " not found."));
     return this.appointmentRepository.findAllByPatientIdOrderByTimeAsc(pat.getId());
   }
 }
