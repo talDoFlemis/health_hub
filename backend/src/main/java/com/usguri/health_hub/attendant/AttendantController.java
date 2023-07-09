da175395d1c85120356547960703a2f5a606cf9a
taldoflemis <coderflemis@gmail.com>
2023-07-09 12:39:24 -0300

    docs: adicionar docs ao package attendante
    adicionar documentacao ao AttendantController
    
    adicionar documentacao ao AttendantService

--- backend/src/main/java/com/usguri/health_hub/attendant/AttendantController.java
+++ backend/src/main/java/com/usguri/health_hub/attendant/AttendantController.java
@@ -9,17 +9,38 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * Controlador responsável por lidar com as operações relacionadas aos atendentes.
+ *
+ * @author André Willyan
+ * @version 1.0
+ * @since 2023-07-02
+ */
 @RestController
 @RequestMapping(path = "${apiPrefix}/attendant")
 @RequiredArgsConstructor
 public class AttendantController {
+
   private final AttendantService attendantService;
 
+  /**
+   * Obtém os dados do atendente autenticado.
+   *
+   * @param auth A autenticação do atendente.
+   * @return Os dados do atendente autenticado.
+   */
   @GetMapping("/me")
   public Attendant getMyData(Authentication auth) {
     return this.attendantService.findByEmail(auth.getName());
   }
 
+  /**
+   * Obtém a lista de todos os atendentes (apenas para ADMIN).
+   *
+   * @param name (Opcional) O nome do atendente para filtrar a busca.
+   * @param email (Opcional) O email do atendente para filtrar a busca.
+   * @return A lista de todos os atendentes.
+   */
   @GetMapping("/all")
   @PreAuthorize(value = "hasAnyRole('ADMIN')")
   public List<Attendant> getAllAttendants(
@@ -27,12 +48,24 @@ public class AttendantController {
     return this.attendantService.getAll(name, email);
   }
 
+  /**
+   * Obtém os dados de um atendente pelo seu ID (apenas para ADMIN).
+   *
+   * @param id O ID do atendente.
+   * @return Os dados do atendente encontrado.
+   */
   @GetMapping("/{id}")
   @PreAuthorize(value = "hasAnyRole('ADMIN')")
   public Attendant getAttendantById(@PathVariable Long id) {
     return this.attendantService.findById(id);
   }
 
+  /**
+   * Registra um novo atendente (apenas para ADMIN).
+   *
+   * @param pat Os dados do atendente a ser registrado.
+   * @return O atendente registrado.
+   */
   @PostMapping("/create")
   @ResponseStatus(HttpStatus.CREATED)
   @PreAuthorize(value = "hasAnyRole('ADMIN')")
@@ -40,6 +73,11 @@ public class AttendantController {
     return this.attendantService.createAttendant(pat);
   }
 
+  /**
+   * Remove um atendente pelo seu ID (apenas para ADMIN).
+   *
+   * @param id O ID do atendente a ser removido.
+   */
   @DeleteMapping("{id}")
   @ResponseStatus(HttpStatus.OK)
   @PreAuthorize(value = "hasAnyRole('ADMIN')")
@@ -47,6 +85,13 @@ public class AttendantController {
     this.attendantService.removeAttendant(id);
   }
 
+  /**
+   * Atualiza os dados de um atendente pelo seu ID (apenas para ADMIN).
+   *
+   * @param pat Os dados atualizados do atendente.
+   * @param id O ID do atendente a ser atualizado.
+   * @return Os dados do atendente atualizado.
+   */
   @PatchMapping("/update/{id}")
   @ResponseStatus(HttpStatus.OK)
   @PreAuthorize(value = "hasAnyRole('ADMIN')")
