2382293c0a7d3ecb1bb631db2cbb8df321fe7a7a
talDoFlemis <talDoFlemis@users.noreply.github.com>
2023-07-09 01:11:54 +0000

    Apply formatting changes

--- backend/src/main/java/com/usguri/health_hub/attendant/AttendantRepository.java
+++ backend/src/main/java/com/usguri/health_hub/attendant/AttendantRepository.java
@@ -1,15 +1,15 @@
 package com.usguri.health_hub.attendant;
 
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
-
-import jakarta.validation.constraints.NotNull;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AttendantRepository extends CrudRepository<Attendant, Long> {
   Optional<Attendant> findById(long id);
 
-  @NotNull List<Attendant> findAll();
+  @NotNull
+  List<Attendant> findAll();
 
   void deleteById(long id);
 
